96326528e77cb1abce6ea96a4d326c2bed3dd1b7
xiter22222 <pawanpediredla@gmail.com>
2024-09-20 20:29:30 +0530

    add ->MAKEFILE

---
+++ .history/build_20240920202807.zig
@@ -0,0 +1,12 @@
+const std=@import("std");
+
+pub fn build(b:*std.build.Builder()) void{
+    const mode = b.standardReleaseOptions();
+    const exe = b.addExecutable("ts-transpiler","src/main.zig");
+    exe.setBuild(exe);
+    exe.setBuildMode(mode);
+    exe.install();   
+    const run_cmd =exe.run();
+    run_cmd.step.dependOn(b.getInstallStep());
+b.default_step.dependOn(&run_cmd.step);
+} 
